c3e0a3e914e112552eb0a050aa32dd1e746008d4
Amal Jose <amaljoseami@gmail.com>
2024-06-14 14:08:57 +0530

    Cleanup

--- src/components/UserTable/index.tsx
+++ src/components/UserTable/index.tsx
@@ -1,19 +1,34 @@
 import React from "react";
 import Loader from "@/components/Loader";
-import { loaderStructure } from "./loaderStructure";
 import { UserDataType } from "@/hooks/useUserData";
 
 type UserTableProps = {
-  loading: boolean;
   data: UserDataType;
 };
 
-export const UserTable = ({ loading, data }: UserTableProps) => {
+const UserTable = ({ data }: UserTableProps) => {
   return (
-    <Loader isLoading={loading} structure={loaderStructure}>
+    <div>
       {data.projects.map((project) => (
         <p key={project.name}>{project.name}</p>
       ))}
+    </div>
+  );
+};
+
+export const loaderStructure = [
+  { type: "text", width: "90%", height: "20px" },
+  { type: "text", width: "70%", height: "20px" },
+  { type: "text", width: "50%", height: "20px" },
+];
+
+export const UserCardWithLoader = ({
+  loading,
+  data,
+}: UserTableProps & { loading: boolean }) => {
+  return (
+    <Loader structure={loaderStructure} isLoading={loading}>
+      <UserTable data={data} />
     </Loader>
   );
 };
