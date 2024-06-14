b0ea016745a3773d0ebc3ae7b686490b767e4aad
Amal Jose <amaljoseami@gmail.com>
2024-06-14 15:23:30 +0530

    Cleanup

--- src/hooks/useUserData.ts
+++ src/hooks/useUserData.ts
@@ -6,7 +6,7 @@ const userData = {
   name: "Amal Jose Alex",
   title: "Senior Frontend Engineer",
   location: "Kochi, Kerala",
-  avatarUrl: "/avatar.jpg",
+  avatarUrl: "/avatar.png",
   projects: [
     "DendroWeb: A research project centered on violin photo recognition, employing skills in Computer Vision, HCI, and CNN.",
     "ShapeRoute: An entrepreneurial initiative for body photo tracking, developed with React Native.",
